0e8644c659a1e4c190f857bf5e030ec048373386
Alejandro Covarrubias <acovarru@live.com>
2014-09-12 09:03:39 -0700

    First commit for P1
    Changed directory of repository..Previous repository name was CSCIE15

---
+++ P1.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<title>Alejandro Covarrubias P1</title>
+
+
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<h1>
+<p>Alejandro Covarrubias</p>
+<p>Dynamic Web Apps Portfolio</p>
+   <img src="/im/head_shot.png" alt="Alejandro Covarrubias" height="178" width="178">   
+</h1>
+
+<h2>   
+<!--A href="">Project2</A>
+<A href="">Project3</A> 
+<A href="">Project4</A-->  
+</h2>
+
+
+<div class="pure-menu pure-menu-open">
+    <a class="pure-menu-heading">Projects</a>
+
+    <ul>
+   
+        <li><a href="#">Project2</a></li>
+        <li><a href="#">Project3</a></li>
+        <li><a href="#">Project4</a></li>
+        
+    </ul>
+</div>
+
+</body>
+<p style="color: black">In 2+ sentences describe who you are and what you do.</p>
+<font size="4" color="gray">I am a software engineer at Sony Network Entertainment,I have been working for 3 years on software automation for IPTV services and currently on transition to start working on software for PS4</font>
+
+
+<p style="color: black">Why are you taking this course?</p>
+
+<font size="4" color="gray">I previously completed CSCI E-55 (Java for distributed computing) and found the course to be really helpful towards improving programming skills and solidifying object oriented programming  and distributed computing concepts. I am looking to pursue Information Technology Graduate Program( Software Engineering concentration) and taking this class in order to fulfill elective requirement </font>
+<p style="color: black">What previous experience do you have with programming?</p>
+<font size="4" color="gray">Object oriented programming: Java, PHP and a little bit of C#. Also related to C and Python</font>
+
+<p style="color: black">Are you working on a Mac, PC, or some other platform?</p>
+<font size="4" color="gray">PC, Windows</font>
+
+<p style="color: black">Anything else we should know?</p>
+<font size="4" color="gray">I am looking forward to learn a lot during this course</font>
+</body>
+
+</html>
---
+++ README.md
@@ -0,0 +1,21 @@
+# Project 1 Dynamic Web Apps Portfolio
+
+## Live URL
+<http://.com>
+
+## Description
+A presentation and portfolio page
+Links to project 2,3 and 4 for Dynamic Web Apps course are included on site
+
+## Demo
+No Demo is required for P1
+
+## Details for teaching team
+No additional details, no login required
+
+
+
+## Outside code
+*Added pure to page via Yahoo's free CDN
+*Added viewport meta to control widht and scale of viewport on mobile browsers
+*Added Pure CSS vertical menu
